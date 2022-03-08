00b5227462241c1c548ed0e552be4ac521229a39
jaidev3 <91534937+jaidev3@users.noreply.github.com>
2022-03-08 10:31:09 +0530

    delete function

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-function CartItem({value1}) {
+function CartItem({value1,value2}) {
 //   const { cartItem } = props;
 console.log({value1})
+const handleClick=()=>{
+  value2(value1)
+}
   return (
     <div>
       <h3>{value1.winery}</h3>
-      <button >delete</button>
+      <button onClick={handleClick}>delete</button>
     </div>
   );
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,19 +5,30 @@ import styled from  'styled-components'
 
 function Cart({value}) {
  const [cart,setCart] =useState([])
+
  useEffect(()=>{
-   setCart(value)
+   
+  setCart(value)
  },[])
 console.log(cart)
+
+const getData=(e)=>{
+
+ setCart(cart.filter((d)=> d.id!=e.id))
+}
+const handleSave=()=>{
+
+}
   return (
     <div>
       <Link to='/'><button>Back to home</button></Link>
 
      {cart.map((e)=>
-      <CartItem value1={e}/>
+      <CartItem value1={e} value2={getData}/>
      )} 
       
       <h1>Total amount:Rs 1000</h1>
+      <button onClick={handleSave}>Save for Next Time</button>
       <Link to='/checkout'><button>Checkout</button></Link>
     </div>
   )
